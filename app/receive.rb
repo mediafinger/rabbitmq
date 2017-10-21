dddbcc0c936729c4cf7d15faf00580d95a74a5dc
Andreas Finger <webmaster@mediafinger.com>
2017-10-21 02:38:48 +0200

    Example 2, add ack to not lose a message when worker dies

--- app/receive.rb
+++ app/receive.rb
@@ -4,10 +4,14 @@ con = Connection.new
 
 begin
   puts " [*] Waiting for messages. To exit press CTRL+C"
-  con.queue.subscribe(block: true) do |_delivery_info, _properties, body|
-    puts " [x] Received #{body}" # -- #{delivery_info} // #{properties}"
+
+  con.queue.subscribe(manual_ack: true, block: true) do |delivery_info, properties, body|
+    puts " [x] Received #{body} -- #{properties}"
     sleep body.count(".").to_i
     puts " [x] Done"
+
+    # send ack that message was received && processed
+    con.channel.ack(delivery_info.delivery_tag)
   end
 rescue Interrupt => _
   con.close
